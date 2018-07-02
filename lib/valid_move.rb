89178e3b2d58e2d90455dd9dfdb6be02a44940ec
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-02 06:18:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(array, index)
   if(index >= 1 && index <= 9)
-    position_taken(array, index)
+    
     return true
   else
     return false
