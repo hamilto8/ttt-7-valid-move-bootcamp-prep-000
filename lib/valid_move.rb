839a1c08815de484fe87c224a56a7f8f522c1d0f
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-02 06:13:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,9 +3,9 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, index)
-if(array[index] == "" || array[index] == " " || array[index] == nil)
-  return false 
-else
-  return true
-end
+  if(array[index] == "" || array[index] == " " || array[index] == nil)
+    return false 
+  else
+    return true
+  end
 end
