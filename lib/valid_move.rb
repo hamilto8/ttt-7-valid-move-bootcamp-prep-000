c449224d38d5ce9affe8866af35576f7e48cd54a
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-02 06:15:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
-def valid_move?
-
+def valid_move?(choice)
+if(choice >= 1 && choice <= 9) 
+  position_taken(array, choice)
+else 
+  return false
+end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
